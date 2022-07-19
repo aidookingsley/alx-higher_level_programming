0c25cfc60f1d82d67b34290e08e26f50f6577740
aidookingsley <kaidoog36@gmail.com>
2022-07-19 14:22:33 -0700

    python class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module Square """
+
+
+class Square:
+    """ Square class defined by geometric shape
+
+        Attributes:
+            size (int): Size of square
+    """
+    def __init__(self, size):
+        """
+        Initialize method
+
+        Args:
+            size (int): size of square
+        """
+        self.__size = size
