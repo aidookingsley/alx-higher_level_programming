3a5a77bd8a549cb802b6a296803dd9af793dc962
kingsley aidoo <kaidoog36@gmail.com>
2022-11-27 20:11:14 +0000

    Update 8-script.js

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,5 +1,5 @@
-$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data, txtStat) {
-  $('ul#list_movies').html(data.results.map(function(res){
-	return `<li>${res.title}</li>`
-	}));
+$.get('https://swapi.co/api/films/?format=json', function (data) {
+  $.each(data.results, function (index, value) {
+    $('<li>' + value.title + '</li>').appendTo('UL#list_movies');
+  });
 });
