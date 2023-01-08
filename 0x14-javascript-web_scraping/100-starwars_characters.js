bed92e82a0f22354307bbcce835eae7da50d5371
aidookingsley <kaidoog36@gmail.com>
2023-01-08 06:58:46 -0800

    web scraping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -4,17 +4,16 @@
 * Prints out who is playing
 */
 
-const req = require('request');
-const id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/';
-req(url + id, function (error, response, body) {
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
   if (error) console.log(error);
   else {
-    const character = JSON.parse(body).characters;
-    character.forEach(element) => {
-      req(element, function (err, res, content) {
-        if (error) console.log(err);
-        console.log(JSON.parse(content).name);
+    const character = JSON.parse(body).character;
+    character.forEach((character) => {
+      request(character, function (error, response, body) {
+        if (error) console.log(error);
+        else console.log(JSON.parse(body).name);
       });
     });
   }
