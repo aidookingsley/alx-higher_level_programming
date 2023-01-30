57ad6d965793245a2b9c4d62a48c37d5c1c1f6b9
aidookingsley <kaidoog36@gmail.com>
2023-01-30 11:53:48 +0000

    correction

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-count = 1
-for w in range(0, 9):
-    for i in range(count, 10):
-        if w == 8 and i == 9:
-            print("{}".format(89))
-            continue
-        print("{}{}, ".format(w, i), end="")
-    count = count + 1    
+for i in range(9):
+    for a in range(i + 1, 10):
+        if i != 8 or a != 9:
+            print("{:n}{:n}".format(i, a), end=", ")
+print("{:n}{:n}".format(i, a))
